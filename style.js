b4a3dc3d68bb70edcd2ea2709ef4bb717aef5a02
AntonGrubkin <elcomendante86@gmail.com>
2023-05-16 22:19:24 +0300

    fix

--- style.js
+++ style.js
@@ -8,3 +8,38 @@ function magic() {
 
 // When scrolling, we run the function
 window.onscroll = magic
+
+const swiperEl = document.querySelector('swiper-container')
+Object.assign(swiperEl, {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  autoplay:true,
+  speed: 600,
+  pagination: false,
+  breakpoints: {
+    360: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    568: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
+swiperEl.initialize();
+
+
+
