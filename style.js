12c1acc7f78337872b415d7544b9f1f6845a3dfb
AntonGrubkin <elcomendante86@gmail.com>
2023-12-22 16:24:22 +0200

    fix_menu

--- style.js
+++ style.js
@@ -6,40 +6,44 @@ function magic() {
   } else { block.style.opacity = '0' }
 }
 
-// When scrolling, we run the function
-window.onscroll = magic
 
-const swiperEl = document.querySelector('swiper-container')
-Object.assign(swiperEl, {
-  slidesPerView: 3,
-  spaceBetween: 20,
-  autoplay:true,
-  speed: 600,
-  pagination: false,
-  breakpoints: {
-    360: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-    568: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 20,
-    },
-  },
+const burger = document.querySelector('.nav-icon');
+const navMenu = document.querySelector('.nav-menu');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    document.querySelector("body").classList.toggle("scroll-hide");
+    document.querySelector("html").classList.toggle("scroll-hide");
+
+})
+
+document.querySelectorAll(".nav-link").forEach((link) =>
+  link.addEventListener("click", () => {
+    burger.classList.remove("active");
+    navMenu.classList.remove("active");
+    document.querySelector("body").classList.remove("scroll-hide");
+    document.querySelector("html").classList.remove("scroll-hide");
+
+  })
+ 
+);
+
+$(document).ready(function() {
+  $(".nav-icon").on("click", function() {
+    $(this).toggleClass("closed");
+  })
+})
+
+
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 80) {
+        $('.header-nav-container').addClass('fixed');
+    } else {
+        $('.header-nav-container').removeClass('fixed');
+    }
 });
-swiperEl.initialize();
 
 
 
